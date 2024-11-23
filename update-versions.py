5997af56ec53965439bd8f94117c633020ec5aaf
guslma <141639311+guslma@users.noreply.github.com>
2024-11-23 09:29:20 -0300

    Update update-versions.py

--- update-versions.py
+++ update-versions.py
@@ -4,24 +4,45 @@ import yaml
 import requests
 
 def get_latest_tag(repo_name):
-    """Obtém a última tag de versão de uma imagem Docker no Docker Hub."""
+    """Obtém a última tag de versão de uma imagem Docker no Docker Hub ou no GitHub para casos específicos."""
+    # Caso especial para o repositório koush/scrypted
+    if repo_name == "koush/scrypted":
+        print(f"Getting latest version of {repo_name} from GitHub")
+        return get_latest_tag_from_github(repo_name)
+    
+    # Busca padrão no Docker Hub
+    print(f"Getting latest version of {repo_name} from Docker Hub")
     tags_url = f'https://hub.docker.com/v2/repositories/{repo_name}/tags'
     response = requests.get(tags_url)
     response.raise_for_status()
+    
     tags = response.json().get('results', [])
     version_tags = [tag['name'] for tag in tags if any(char.isdigit() for char in tag['name'])]
-    return sorted(version_tags, reverse=True)[0] if version_tags else None
+    
+    if version_tags:
+        latest_tag = sorted(version_tags, reverse=True)[0]
+        print(f"Latest version of {repo_name} is {latest_tag}")
+        return latest_tag
+    else:
+        print(f"No version tags found for {repo_name} on Docker Hub.")
+        return None
 
-def process_file(file_path, file_format):
-    """Processa o arquivo de configuração, atualizando a versão da imagem."""
-    with open(file_path, 'r') as f:
-        if file_format == 'yaml':
-            config = yaml.safe_load(f)
-        elif file_format == 'json':
-            config = json.load(f)
-        else:
-            print(f"Formato não suportado para {file_path}")
-            return
+def get_latest_tag_from_github(repo_name):
+    """Obtém a última tag de versão de um repositório no GitHub."""
+    tags_url = f'https://api.github.com/repos/{repo_name}/tags'
+    response = requests.get(tags_url)
+    response.raise_for_status()
+    
+    tags = response.json()
+    version_tags = [tag['name'] for tag in tags if any(char.isdigit() for char in tag['name'])]
+    
+    if version_tags:
+        latest_tag = sorted(version_tags, reverse=True)[0]
+        print(f"Latest version of {repo_name} on GitHub is {latest_tag}")
+        return latest_tag
+    else:
+        print(f"No version tags found for {repo_name} on GitHub.")
+        return None
 
     if 'image' in config:
         print(f"Getting latest version of {config['image']} from Docker Hub")
