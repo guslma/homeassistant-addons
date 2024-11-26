bda5a04a08758d879d240e1d7981540107cbe662
guslma <141639311+guslma@users.noreply.github.com>
2024-11-26 10:41:29 -0300

    Update update-versions.py

--- update-versions.py
+++ update-versions.py
@@ -13,9 +13,7 @@ def get_latest_tag(repo_name):
         # Filtra as tags que têm números e não contêm "nvidia" ou "lite"
         version_tags = [
             tag['name'] for tag in tags
-            if any(char.isdigit() for char in tag['name']) and
-               "nvidia" not in tag['name'].lower() and
-               "lite" not in tag['name'].lower()
+            if any(char.isdigit() for char in tag['name'])
         ]
         return sorted(version_tags, reverse=True)[0] if version_tags else None
     except requests.exceptions.RequestException as e:
