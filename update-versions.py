18fc33152fedb400bebc67747e4080007871ae05
guslma <141639311+guslma@users.noreply.github.com>
2024-11-26 16:09:00 -0300

    Update update-versions.py

--- update-versions.py
+++ update-versions.py
@@ -3,24 +3,34 @@ import json
 import yaml
 import requests
 
+# Coloque seu token de acesso pessoal aqui
+GITHUB_TOKEN = 'seu_token_aqui'
+
 def get_latest_tag(repo_name):
     """Obtém a última tag de versão de uma imagem Docker no Docker Hub ou no GitHub Container Registry (GHCR)."""
     
     # Verifica se o repositório é do GitHub Container Registry (GHCR) ou Docker Hub
     if repo_name.startswith('ghcr.io'):
         tags_url = f'https://ghcr.io/v2/{repo_name}/tags/list'
+        headers = {
+            'Authorization': f'Bearer {GITHUB_TOKEN}'
+        }
     else:
         tags_url = f'https://hub.docker.com/v2/repositories/{repo_name}/tags'
-    
+        headers = {}
+
     try:
-        response = requests.get(tags_url)
+        response = requests.get(tags_url, headers=headers)
         response.raise_for_status()  # Levanta uma exceção se houver erro na requisição
         tags = response.json().get('tags', []) if 'ghcr.io' in tags_url else response.json().get('results', [])
         
         # Filtra as tags que têm números e não contêm "nvidia" ou "lite"
         version_tags = [
             tag for tag in tags
-            if any(char.isdigit() for char in tag)]
+            if any(char.isdigit() for char in tag) and
+               "nvidia" not in tag.lower() and
+               "lite" not in tag.lower()
+        ]
         return sorted(version_tags, reverse=True)[0] if version_tags else None
     except requests.exceptions.RequestException as e:
         print(f"Erro ao acessar o repositório {repo_name}: {e}")
