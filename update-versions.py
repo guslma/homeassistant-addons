90fd1b10e17395ab79fac677f4872595cfced8c0
guslma <141639311+guslma@users.noreply.github.com>
2024-11-29 10:01:30 -0300

    Update update-versions.py

--- update-versions.py
+++ update-versions.py
@@ -3,8 +3,8 @@ import json
 import yaml
 import requests
 
-def get_latest_and_previous_tag(repo_name):
-    """Obtém a última tag de versão e a versão anterior de uma imagem Docker no Docker Hub."""
+def get_latest_tag(repo_name):
+    """Obtém a última tag de versão de uma imagem Docker no Docker Hub."""
     tags_url = f'https://hub.docker.com/v2/repositories/{repo_name}/tags'
     try:
         response = requests.get(tags_url)
@@ -14,37 +14,19 @@ def get_latest_and_previous_tag(repo_name):
         # Filtra as tags que têm números e não contêm "nvidia" ou "lite"
         version_tags = [
             tag['name'] for tag in tags
-            if any(char.isdigit() for char in tag['name']) and
-               "lite" not in tag['name'].lower() and
+            if any(char.isdigit() for char in tag['name']) 
+            and
+               "latest" not in tag['name'].lower()
+            and
+               "lite" not in tag['name'].lower()
+            and
                "nvidia" not in tag['name'].lower()
         ]
 
-        if not version_tags:
-            print(f"Nenhuma tag válida encontrada para {repo_name}.")
-            return None, None
-
-        # Ordena as tags em ordem decrescente
-        version_tags = sorted(version_tags, reverse=True)
-
-        # Verifica se a tag "latest" está na lista
-        if 'latest' in version_tags:
-            latest_index = version_tags.index('latest')
-            # Verifica se há uma tag anterior à "latest"
-            if latest_index > 0:
-                return version_tags[latest_index - 1], version_tags[latest_index]
-            else:
-                print("Não há versão anterior à 'latest'.")
-                return None, version_tags[latest_index]
-        elif version_tags:
-            # Retorna a última e a segunda última versão
-            return version_tags[0], version_tags[1] if len(version_tags) > 1 else None
-        else:
-            print(f"Nenhuma tag encontrada para {repo_name}.")
-            return None, None
-
+        return sorted(version_tags, reverse=True)[0] if version_tags else None
     except requests.exceptions.RequestException as e:
         print(f"Erro ao acessar o Docker Hub: {e}")
-        return None, None
+        return None
 
 def process_file(file_path, file_format):
     """Processa o arquivo de configuração, atualizando a versão da imagem."""
@@ -60,19 +42,17 @@ def process_file(file_path, file_format):
 
         if 'image' in config and 'version' in config:
             print(f"Obtendo a última versão da imagem {config['image']} do Docker Hub")
-            previous_tag, latest_tag = get_latest_and_previous_tag(config['image'])
-            if previous_tag:
-                print(f"A versão anterior à 'latest' para {config['image']} é {previous_tag}")
-                if config['version'] != previous_tag:
-                    print(f"Atualizando {file_path} de {config['version']} para {previous_tag}")
-                    config['version'] = previous_tag
+            latest_tag = get_latest_tag(config['image'])
+            if latest_tag:
+                print(f"Última versão de {config['image']} é {latest_tag}")
+                if config['version'] != latest_tag:
+                    print(f"Atualizando {file_path} de {config['version']} para {latest_tag}")
+                    config['version'] = latest_tag
                     with open(file_path, 'w') as f:
                         if file_format == 'yaml':
                             yaml.dump(config, f, default_flow_style=False, sort_keys=False)
                         elif file_format == 'json':
                             json.dump(config, f, indent=2)
-            else:
-                print(f"Não foi possível encontrar uma versão anterior à 'latest' para {config['image']}.")
         else:
             print(f"Arquivo {file_path} não contém 'image' ou 'version'.")
     except Exception as e:
