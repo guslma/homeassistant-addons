8f2db3103be3fc913f7d0e5acf67df43c04bb72b
Gustavo Lima <141639311+guslma@users.noreply.github.com>
2025-09-05 09:26:54 -0300

    Update update-versions.py

--- update-versions.py
+++ update-versions.py
@@ -4,40 +4,34 @@ import yaml
 import requests
 import re
 
-def get_latest_and_previous_tag(repo_name):
+def get_latest_and_previous_tag(repo_owner, repo_name):
     """Obtém a tag 'latest' e a versão anterior de uma imagem Docker no Docker Hub, ignorando tags indesejadas."""
-    tags_url = f'https://hub.docker.com/v2/repositories/{repo_name}/tags?page_size=50'
+    tags_url = f'https://api.github.com/repos/{repo_owner}/{repo_name}/tags'
     try:
         response = requests.get(tags_url)
         response.raise_for_status()  # Levanta uma exceção se houver erro na requisição
-        tags = response.json().get('results', [])
+        tags = response.json()
         
         # Filtra tags válidas: devem conter números e não podem conter padrões indesejados (ex.: -nvidia)
-        excluded_patterns = ['nvidia', 'beta', 'lite', 'router', 'intel', 'latest_nohealthcheck']  # Adicione padrões a serem excluídos
+        excluded_patterns = ['beta']  # Adicione padrões a serem excluídos
         version_tags = [
-            tag['name']
+            tag["name"]
             for tag in tags
-            if re.search(r'\d', tag['name']) and not any(excl in tag['name'] for excl in excluded_patterns)
+            if re.search(r'\d', tag["name"]) and not any(excl in tag["name"] for excl in excluded_patterns)
         ]
 
         if not version_tags:
-            print(f"Nenhuma tag válida encontrada para {repo_name}.")
+            print(f"Nenhuma tag válida encontrada para {repo_owner}/{repo_name}.")
             return None, None
 
         # Ordena as tags alfabeticamente em ordem decrescente
         version_tags = sorted(version_tags, reverse=True)
 
-        # Verifica se a tag "latest" está na lista de todas as tags
-        all_tags = [tag['name'] for tag in tags]
-        if 'latest' in all_tags:
-            # Retorna a tag mais recente válida e a imediatamente anterior
-            return version_tags[0], 'latest'
-        else:
-            print(f"A tag 'latest' não foi encontrada para {repo_name}.")
-            return version_tags[0], None
+        # Retorna a tag mais recente válida e a imediatamente anterior
+        return version_tags[0], None
 
     except requests.exceptions.RequestException as e:
-        print(f"Erro ao acessar o Docker Hub: {e}")
+        print(f"Erro ao acessar o GitHub: {e}")
         return None, None
 
 def process_file(file_path, file_format):
@@ -53,12 +47,14 @@ def process_file(file_path, file_format):
                 return
 
         if 'image' in config and 'version' in config:
-            print(f"Obtendo a última versão da imagem {config['image']} do Docker Hub")
-            previous_tag, latest_tag = get_latest_and_previous_tag(config['image'])
+            repo_owner, repo_name = config["image"].split("/")
+            print(f"Obtendo a última versão da imagem {repo_owner}/{repo_name} do GitHub")
+            previous_tag, latest_tag = get_latest_and_previous_tag(repo_owner, repo_name)
+
             if previous_tag:
-                print(f"A versão anterior à 'latest' de {config['image']} é {previous_tag}")
-                if config['version'] != previous_tag:
-                    print(f"Atualizando {file_path} de {config['version']} para {previous_tag}")
+                print(f'A versão mais recente de {config["image"]} é {previous_tag}')
+                if config["version"] != previous_tag:
+                    print(f"Atualizando {file_path} de {config["version"]} para {previous_tag}")
                     config['version'] = previous_tag
                     with open(file_path, 'w') as f:
                         if file_format == 'yaml':
